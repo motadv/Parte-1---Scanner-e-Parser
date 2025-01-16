23192e9c7d49b0087c9b2248d52df74a8a9bfe0d
Maximilian Harrisson <maximilianharrisson@gmail.com>
2025-01-16 01:10:54 -0300

    Add code generation for basic tokens

--- src/code_generator.py
+++ src/code_generator.py
@@ -21,7 +21,7 @@ class CodeGenerator:
 
         local_output = []
 
-        print(f"Type: {node.token.type_}, Value: {node.token.value}")
+        # print(f"Type: {node.token.type_}, Value: {node.token.value}")
         if node.token.value == EMPTY_CHAR:
             return local_output
         # Done
@@ -32,6 +32,8 @@ class CodeGenerator:
         elif node.token.type_ == "<MAIN>":
             local_output.append(create_label(node.children[1].token.value, node.children[6].token.value))
             local_output.extend(self.cgen(node.children[14], variables))
+            local_output.append("li $v0, 10")
+            local_output.append("syscall")
         # Done
         elif node.token.type_ == "<CMD>":
             if node.children[0].token.value == "{":
@@ -192,173 +194,23 @@ class CodeGenerator:
             elif first.token.value == "new":
                 local_output.extend(self.cgen(node.children[1], variables))
             elif first.token.type_ == "<PEXP>":
-                local_output.extend(self.cgen(node.children[0], variables))
-                local_output.extend(self.cgen(node.children[1], variables))
-            child = node.children[1]
-            if child.children[0].token.value != EMPTY_CHAR:
-                local_output.append("sw $a0, 0($sp)")
-                local_output.append("addiu $sp, $sp, -4")
-                local_output.extend(self.cgen(child, variables))
-                local_output.append("lw $t1, 4($sp)")
-                operator = child.children[0].token.value
-                if operator == "<":
-                    local_output.append("slt $a0, $t1, $a0")
-                elif operator == "==":
-                    local_output.append("seq $a0, $t1, $a0")
-                elif operator == "!=":
-                    local_output.append("sne $a0, $t1, $a0")
-                local_output.append("addiu $sp, $sp, 4")
-        elif node.children[0].token.type_ == "<CONSTANT>":
-            local_output.append(f"li $a0, {node.children[0].children[0].token.value}")
+                if first.children[0].token.value == "(":
+                    local_output.extend(self.cgen(first.children[1], variables))
+                elif first.children[0].token.type_ == "identifier":
+                    name = first.children[0].token.value
+                    # TODO: use variable
+                elif first.children[0].token.value == "this":
+                    # TODO: use local variable
+                    pass
+        elif node.token.type_ == "<OEXPS>":
+            # TODO: add parameters
+            pass
+        elif node.token.type_ == "<CONSTANT>":
+            local_output.append(f"li $a0, {node.children[0].token.value}")
         return local_output
-
-# %%
-# def generate_program(self, semantic_tree: Node):
-#     self.output.append(".data")
-#     for element in semantic_tree:
-#         if element["type"] == "class":
-#             self.generate_class(element)
-#
-#     self.output.extend(self.data_section)
-#     self.output.extend(self.text_section)
-#
-# def generate_class(self, class_node):
-#     if "main" in class_node:
-#         self.generate_main(class_node["main"])
-#
-# def generate_main(self, main_node):
-#     self.text_section.append("main:")
-#     for cmd in main_node.get("commands", []):
-#         self.generate_command(cmd)
-#     self.text_section.append("    li $v0, 10")
-#     self.text_section.append("    syscall")
-#
-# def generate_command(self, cmd_node):
-#     if cmd_node.get("type") == "assignment":
-#         target = cmd_node["target"]
-#         expression = self.generate_expression(cmd_node["expression"])
-#         self.text_section.append(f"    li $t0, {expression}")
-#         self.text_section.append(f"    sw $t0, {target}")
-#         self.data_section.append(f"{target}: .word 0")
-#
-# def generate_expression(self, expr_node):
-#     if expr_node.get("type") == "constant":
-#         return expr_node["value"]
-#     elif expr_node.get("type") == "identifier":
-#         return f"{expr_node['name']}"
-#     elif expr_node.get("type") == "operation":
-#         left = self.generate_expression(expr_node["left"])
-#         right = self.generate_expression(expr_node["right"])
-#         op = expr_node["operator"]
-#
-#         if op == "+":
-#             self.text_section.append(f"    li $t1, {left}")
-#             self.text_section.append(f"    li $t2, {right}")
-#             self.text_section.append("    add $t0, $t1, $t2")
-#         elif op == "-":
-#             self.text_section.append(f"    li $t1, {left}")
-#             self.text_section.append(f"    li $t2, {right}")
-#             self.text_section.append("    sub $t0, $t1, $t2")
-#         elif op == "*":
-#             self.text_section.append(f"    li $t1, {left}")
-#             self.text_section.append(f"    li $t2, {right}")
-#             self.text_section.append("    mul $t0, $t1, $t2")
-#         elif op == "/":
-#             self.text_section.append(f"    li $t1, {left}")
-#             self.text_section.append(f"    li $t2, {right}")
-#             self.text_section.append("    div $t0, $t1, $t2")
-#
-#         return "$t0"
-#
-# def get_output(self):
-#     return "\n".join(self.output)
 
 
 def generate_code(symbol_table: dict, semantic_tree: Node):
-
-    # symbol_table = {
-    #     "A": {
-    #         "type": "class",
-    #         "extends": None,
-    #         "variables": {
-    #             "this": {
-    #                 "type": "A"
-    #             },
-    #             "num1": {
-    #                 "type": "int"
-    #             },
-    #             "num2": {
-    #                 "type": "int"
-    #             },
-    #             "num3": {
-    #                 "type": "int"
-    #             },
-    #             "result": {
-    #                 "type": "int"
-    #             }
-    #         },
-    #         "methods": {
-    #             "foo": {
-    #                 "type": "int",
-    #                 "params": {
-    #                     "p1": {
-    #                         "type": "int"
-    #                     },
-    #                     "p2": {
-    #                         "type": "int"
-    #                     },
-    #                     "p3": {
-    #                         "type": "int"
-    #                     }
-    #                 },
-    #                 "variables": {}
-    #             },
-    #             "bar": {
-    #                 "type": "B",
-    #                 "params": {},
-    #                 "variables": {}
-    #             }
-    #         }
-    #     },
-    #     "B": {
-    #         "type": "class",
-    #         "extends": None,
-    #         "variables": {
-    #             "this": {
-    #                 "type": "B"
-    #             },
-    #             "a": {
-    #                 "type": "A"
-    #             },
-    #             "returnVal": {
-    #                 "type": "int"
-    #             }
-    #         },
-    #         "methods": {
-    #             "foo": {
-    #                 "type": "int",
-    #                 "params": {},
-    #                 "variables": {}
-    #             }
-    #         }
-    #     },
-    #     "C": {
-    #         "type": "class",
-    #         "extends": None,
-    #         "variables": {
-    #             "this": {
-    #                 "type": "C"
-    #             }
-    #         },
-    #         "methods": {
-    #             "foo": {
-    #                 "type": "int",
-    #                 "params": {},
-    #                 "variables": {}
-    #             }
-    #         }
-    #     }
-    # }
 
     code_generator = CodeGenerator(symbol_table)
 
